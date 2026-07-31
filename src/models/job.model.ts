84f5352354ac5f9f791f4e2604a975452ed0a277
Ashwani Kumar <ashwanikr22222@gmail.com>
2026-07-31 11:36:12 +0530

    feat: implement job model and refactor job creation logic

---
+++ src/models/job.model.ts
@@ -0,0 +1,61 @@
+import { Schema, model, Document } from "mongoose";
+
+export enum JobStatus {
+  QUEUED = "QUEUED",
+  PROCESSING = "PROCESSING",
+  COMPLETED = "COMPLETED",
+  FAILED = "FAILED",
+}
+
+export interface IJob extends Document {
+  name: string;
+  email: string;
+  queueJobId?: string;
+  status: JobStatus;
+  attempts: number; //retry count
+  error?: string; //SMTP Authentication Failed
+}
+
+const jobSchema = new Schema<IJob>(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      lowercase: true,
+      trim: true,
+    },
+
+    queueJobId: {
+      type: String,
+    },
+
+    status: {
+      type: String,
+      enum: Object.values(JobStatus),
+      default: JobStatus.QUEUED,
+    },
+
+    attempts: {
+      type: Number,
+      default: 0,
+    },
+
+    error: {
+      type: String,
+      default: null,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Job = model<IJob>("Job", jobSchema);
+
+export default Job;.021
--- src/services/job.service.ts
+++ src/services/job.service.ts
@@ -1,6 +1,7 @@
 //producer hai ya ,producer ka kam hai ki job ko queue me dalna. 
 
 
+import Job from "../models/job.model";
 import { jobQueue } from "../queues/job.queue";
 
 export interface CreateJobData {
@@ -10,7 +11,22 @@ export interface CreateJobData {
 
 class JobService {
   async createJob(data: CreateJobData) {
-    const job = await jobQueue.add("send-email", data);
+
+    const job = await Job.create({
+      name: data.name,
+      email: data.email,
+    });
+
+    const queueJob = await jobQueue.add(
+      "send-email",
+      {
+        name: data.name,
+        email: data.email,
+      }
+    );
+
+    job.queueJobId = queueJob.id;
+    await job.save();
 
     return job;
   }
